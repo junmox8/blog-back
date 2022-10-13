9f8cda53550d1b78dd1a36f33eb1a47d7fe4b32a
junmox8 <931097192@qq.com>
2022-10-13 16:40:53 +0800

    10-13-1

--- controllers/Message.js
+++ controllers/Message.js
@@ -1,6 +1,7 @@
 const { Op } = require("sequelize");
 const sequelize = require("../sequelize/mysql_sequelize");
 const moment = require("moment");
+const dayjs = require("dayjs");
 const { User, Message, MessageAttach } = require("../models/index");
 const { returnSuccess, returnFail } = require("../utils/http");
 const handUpMessage = async (req, res) => {
@@ -9,10 +10,61 @@ const handUpMessage = async (req, res) => {
   const result = await Message.create({
     content,
     userId,
+    created_at: dayjs(new Date()).format("YYYY-MM-DD HH:mm:ss"),
   });
   if (result) returnSuccess(res, "");
   else returnFail(res, "添加留言失败");
 };
+const getAllMessageNumber = async (req, res) => {
+  const result = await Message.findAll();
+  returnSuccess(res, result.length);
+};
+const getOnePageMessage = async (req, res) => {
+  const result = await Message.findAll({
+    include: [User],
+    limit: 5,
+    offset: (req.query.page - 1) * 5,
+  });
+  returnSuccess(res, result);
+};
+const handUpMessageAttach = async (req, res) => {
+  const { content, toUserId, messageId } = req.body;
+  const { id: fromUserId } = req.auth;
+  const result = await MessageAttach.create({
+    content,
+    fromUserId,
+    toUserId,
+    messageId,
+  });
+  if (result) returnSuccess(res, "");
+  else returnFail(res, "回复消息失败");
+};
+const getTheMessageAttach = async (req, res) => {
+  const { id } = req.query;
+  const result = await MessageAttach.findAll({
+    where: {
+      messageId: id,
+    },
+  });
+  if (result) returnSuccess(res, result);
+};
+const handUpMessageAttachAttach = async (req, res) => {
+  const { content, toUserId, messageId } = req.body;
+  const { id: fromUserId } = req.auth;
+  const result = await MessageAttach.create({
+    content,
+    fromUserId,
+    toUserId,
+    messageId,
+  });
+  if (result) returnSuccess(res, result);
+  else returnFail(res, "添加评论失败");
+};
 module.exports = {
   handUpMessage,
+  getAllMessageNumber,
+  getOnePageMessage,
+  handUpMessageAttach,
+  getTheMessageAttach,
+  handUpMessageAttachAttach,
 };
--- routes/message.js
+++ routes/message.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const { Message } = require("../controllers/index");
-const handUpMessage = (req, res) => {
+router.post("/handUpMessage", (req, res) => {
   Message.handUpMessage(req, res);
-};
+});
+router.get("/getAllMessageNumber", (req, res) => {
+  Message.getAllMessageNumber(req, res);
+});
+router.get("/getOnePageMessage", (req, res) => {
+  Message.getOnePageMessage(req, res);
+});
+router.post("/handUpMessageAttach", (req, res) => {
+  Message.handUpMessageAttach(req, res);
+});
+router.get("/getTheMessageAttach", (req, res) => {
+  Message.getTheMessageAttach(req, res);
+});
+router.post("/handUpMessageAttachAttach", (req, res) => {
+  Message.handUpMessageAttachAttach(req, res);
+});
 module.exports = router;
